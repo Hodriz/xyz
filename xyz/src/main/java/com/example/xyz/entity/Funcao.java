994ad9abce8fbfcc117e0d8b7b89ba9d9e032616
Hodriz <rodrigod.borghi@hotmail.com>
2026-02-26 11:56:18 -0300

    firts commit

---
+++ xyz/src/main/java/com/example/xyz/entity/Funcao.java
@@ -0,0 +1,8 @@
+package com.example.xyz.entity;
+
+public enum Funcao {
+    PASSEIO,
+    TRANSPORTE,
+    ESPORTIVO,
+    UTILITARIO
+}
---
+++ xyz/src/main/java/com/example/xyz/entity/Veiculo.java
@@ -0,0 +1,70 @@
+package com.example.xyz.entity;
+
+public class Veiculo {
+    private Long id;
+    private String modelo;
+    private String marca;
+    private Funcao funcao;
+    private int lugares;
+    private int ano;
+
+    public Veiculo(Long id, String modelo, String marca, Funcao funcao, int lugares, int ano) {
+        this.id = id;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.funcao = funcao;
+        this.lugares = lugares;
+        this.ano = ano;
+    }
+
+    public Veiculo() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Funcao getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(Funcao funcao) {
+        this.funcao = funcao;
+    }
+
+    public int getLugares() {
+        return lugares;
+    }
+
+    public void setLugares(int lugares) {
+        this.lugares = lugares;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
